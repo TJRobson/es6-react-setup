f287e76a673bf3239d26c61941629ae1483b92d9
TJRobson <thomas.j.robson@hotmail.com>
2016-11-05 21:44:37 +1300

    egghead.io react lesson 5 & 6

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
 class App extends React.Component {
-  render(){
-    return <div>Hello World</div>
+
+  constructor(){
+    super();
+    this.state = { txt: 'this is the state txt'}
+    this.update = this.update.bind(this)
   }
+
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+
+  render(){
+    return (
+    <div>
+      <Widget txt={this.state.txt} update={this.update} />
+    </div>
+    )
+  }
+}
+
+const Widget = (props) => {
+  return ( <div>
+    <input type='text'
+      onChange={props.update} />
+    <h1>{props.txt}</h1>
+    </div>
+  )
 }
 
 export default App
